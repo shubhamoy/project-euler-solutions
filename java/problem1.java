aa99fe56c8ede87c368d41b19b55e60b4c33d8b4
Shubhamoy Chakrabarty <shubhamoy@kalakhatta.com>
2014-08-26 14:56:54 +0530

    Added solution in Java

---
+++ java/problem1.java
@@ -0,0 +1,15 @@
+class problem1
+{
+	public static void main(String args[])
+	{
+		int sum=0;
+		for(int i=0; i<=999;i=i+1)
+		{
+			if( i % 3 == 0 || i % 5 == 0)
+			{
+				sum = sum+i;
+			}
+		}
+		System.out.println(sum);
+	}
+}
