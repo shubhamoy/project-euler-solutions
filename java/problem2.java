b5d82ac90fb28ffaeb2697e975f393598ed313de
Shubhamoy Chakrabarty <shubhamoy@kalakhatta.com>
2014-08-26 18:59:37 +0530

    Solution of Problem 2 in Java

---
+++ java/problem2.java
@@ -0,0 +1,22 @@
+class problem2
+{
+	public static void main(String args[])
+	{
+		int sum=0; 
+		int num1=1; 
+		int num2=2; 
+		int temp=1;
+
+		while(num2 <= 4000000)
+		{
+			if(num2%2==0)
+			{
+				sum+=num2;
+			}
+			temp = num1;
+			num1 = num2;
+			num2 += temp;
+		}
+		System.out.println(sum);
+	}
+}
