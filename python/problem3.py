e5579c95bfc595f5871b98809e1a3709ffbd3a6d
Shubhamoy <shubhamoy.chakrabarty@gmail.com>
2015-11-07 22:46:19 +0530

    Improved the prime number checking function

--- python/problem3.py
+++ python/problem3.py
@@ -1,9 +1,21 @@
 import math, re
 
-def isPrime(num):
-        pattern = r'^1?$|^(11+?)\1+$'
-        if re.match(pattern, '1'*num) is None:
-                return 1
+def isPrime(n):
+
+	if n == 2: return 1
+	if n == 3: return 1
+	if n % 2 == 0: return 0
+	if n % 3 == 0: return 0
+
+	i = 5
+	w = 2
+	while i * i <= n:
+		if n % i == 0:
+			return 0
+		i += w
+		w = 6 - w
+	return 1
+
 
 n = 600851475143
 b = math.sqrt(n)
