f3517fba493b33d55ff6249112425406d8641e4d
Shubhamoy <shubhamoy.chakrabarty@gmail.com>
2015-11-19 10:29:35 +0530

    Changed class name

--- java/problem2.java
+++ java/problem2.java
@@ -1,16 +1,12 @@
-class problem2
-{
-	public static void main(String args[])
-	{
+class Problem2{
+	public static void main(String args[]){
 		int sum=0; 
 		int num1=1; 
 		int num2=2; 
 		int temp=1;
 
-		while(num2 <= 4000000)
-		{
-			if(num2%2==0)
-			{
+		while(num2 <= 4000000){
+			if(num2%2==0){
 				sum+=num2;
 			}
 			temp = num1;
