86eb249f72edf6674544f65fc225be65144814b5
Shubhamoy <shubhamoy.chakrabarty@gmail.com>
2015-11-19 10:29:01 +0530

    Changed class name

--- java/problem1.java
+++ java/problem1.java
@@ -1,12 +1,8 @@
-class problem1
-{
-	public static void main(String args[])
-	{
+class Problem1{
+	public static void main(String args[]){
 		int sum=0;
-		for(int i=0; i<=999;i=i+1)
-		{
-			if( i % 3 == 0 || i % 5 == 0)
-			{
+		for(int i=0; i<=999;i=i+1){
+			if( i % 3 == 0 || i % 5 == 0){
 				sum = sum+i;
 			}
 		}
