9af99c4371ff565d5d8b13fe2fbaaafa5a29da51
Shubhamoy <me@shubhamoy.com>
2017-06-14 00:27:58 +0530

    Fixed indentation

--- php/problem4.php
+++ php/problem4.php
@@ -1,19 +1,20 @@
 <?php
-	$n1 = 999;
-	$n2 = 999;
-	$res = array();
-	for($i=999;$i>0;$i--){
-		for($j=1;$j<=1000;$j++){
-			$r = $n1 * $n2;
-			if(strrev($r) == $r){
-				if(!in_array($r, $res)){
-					$res[] = $r;
-				}
-			}
-			$n1--;
-		}
-		$n2--;
-		$n1=999;
-	}
-	sort($res);
-	echo $res[count($res)-1];
+  $n1 = 999;
+  $n2 = 999;
+  $res = array();
+  
+  for($i=999;$i>0;$i--){
+    for($j=1;$j<=1000;$j++){
+      $r = $n1 * $n2;
+      if(strrev($r) == $r){
+        if(!in_array($r, $res)){
+          $res[] = $r;
+        }
+      }
+      $n1--;
+    }
+    $n2--;
+    $n1=999;
+  }
+  sort($res);
+  echo $res[count($res)-1];
